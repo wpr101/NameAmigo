e46567f639b94eb6beb2d60c56cafd4f7b227660
William Ross <wpr101@hotmail.com>
2016-02-28 10:19:09 +0200

    renaming

--- web/app/NameCraft.py
+++ web/app/NameCraft.py
@@ -9,17 +9,15 @@ app = Flask(__name__)
 def index():
     
     two_word_names = twp.create_names()
-    one_word_names = owp.create_names_with_meanings()
     describe_projects = cw.create_thesaurus_name()
 
     return render_template('index.html', two_word_names = two_word_names,
-                           one_word_names = one_word_names,
                            describe_projects = describe_projects)
 
 @app.route('/TwoWordProjects')
 def TwoWordProjects():
     project_names = twp.create_names()
-    return render_template('twowordprojects.html', project_names = project_names
-                           )
+    return render_template('twowordprojects.html', project_names = project_names)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- web/cmd.sh
+++ web/cmd.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$ENV" = 'DEV' ]; then
   echo "Running Development Server"
-  exec python "identidock.py"
+  exec python "NameCraft.py"
 else
   echo "Running Production Server"
   exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
