796147d735a09ac18ffe0d7fdedaf7aa08f35b15
William Ross <wpr101@hotmail.com>
2016-03-03 16:57:03 +0200

    procfile

--- web/app/NameCraft.py
+++ web/app/NameCraft.py
@@ -24,4 +24,4 @@ def CustomName(user_word):
                            user_word = user_word)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=33507)
