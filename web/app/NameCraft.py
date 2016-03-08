a806f762dad36af750bfebeca41a76cbcfc05caa
William Ross <wpr101@hotmail.com>
2016-03-08 02:08:53 +0200

    debugging removed

--- web/app/NameCraft.py
+++ web/app/NameCraft.py
@@ -52,4 +52,4 @@ def CustomWords():
 if __name__ == '__main__':
     #app.run(debug=True, host='0.0.0.0', port=33507)
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
