e433f90b954c0826ae20286913af5d6802608451
William Ross <wpr101@hotmail.com>
2016-03-09 19:15:11 +0200

    refactor into PrepareWords out of routing

--- web/app/NameCraft.py
+++ web/app/NameCraft.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, redirect
-import os, string
-#import nltk
-#from nltk.corpus import stopwords
+import os
 
 import ChooseWord as cw
 import CustomNames as cn
+import PrepareWords as pw
 
 app = Flask(__name__)
 
@@ -28,40 +27,14 @@ def CustomName(user_word):
 
 @app.route('/CustomWords', methods=['GET'])
 def CustomWords():
-	user_word = request.args['words']
-	#translator = str.maketrans({key: None for key in string.punctuation})
-	#user_word = user_word.translate(translator)
-	punct = set(string.punctuation)
-	user_word = ''.join(x for x in user_word if x not in punct)
+    user_word = request.args['words']
 
-	word_list = user_word.split()
-	for i in range(len(word_list)):
-	    word_list[i] = word_list[i].capitalize()
-		
-        #remove duplicates from the list
-	word_list = list(set(word_list))
+    word_list = pw.clean_words(user_word)
+    redirect_string = pw.create_redirect_string(word_list)
 	
-	stop = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn']
-	for s in stop:
-	    for w in word_list:
-                if (s.capitalize() == w):
-                    word_list.remove(w)
+    return redirect(redirect_string)
 	
-		
-	redirect_string = '/'
-	for i in range(len(word_list)):
-	    redirect_string += word_list[i] 
-	    redirect_string += "-"
-	#remove the trailing slash
-	redirect_string = redirect_string[:-1]
-	
-	word_list = user_word.split('-')
-	
-	return redirect(redirect_string)
-	
-
-
 if __name__ == '__main__':
     #app.run(debug=True, host='0.0.0.0', port=33507)
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
