9c298f57dc3345cc48316b25efdcbc19af2dc242
wpr101 <wpr101@hotmail.com>
2016-03-17 13:31:27 +0200

    gevent WSGI server added

--- web/app/NameCraft.py
+++ web/app/NameCraft.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, Response
 import os
+from gevent.pywsgi import WSGIServer
 
 import ChooseWord as cw
 import CustomNames as cn
@@ -46,4 +47,5 @@ def SiteMap():
 if __name__ == '__main__':
     #app.run(debug=True, host='0.0.0.0', port=33507)
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=port)
+    WSGIServer(('', port), app).serve_forever()
